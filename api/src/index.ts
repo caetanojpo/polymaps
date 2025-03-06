66e547433ed26e788ced740aed35b10baedd2351
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-03-06 18:26:45 -0300

    feature(app): index

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,9 @@
-import './database';
+import app from "./app";
+import {config} from "dotenv";
+import Server from "./server";
+import {ENV} from "./config/env";
+
+config();
+
+const server = new Server(app, ENV.PORT.toString());
+server.start();
