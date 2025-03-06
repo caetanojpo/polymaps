5574425e525b835feb62316a9f086e36f9588762
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-03-06 18:27:32 -0300

    feature(utils): implemented env config

---
+++ api/src/config/env.ts
@@ -0,0 +1,12 @@
+import {config} from "dotenv";
+
+config();
+
+export const ENV = {
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || "dev",
+    LOG_LEVEL: process.env.LOG_LEVEL || "info",
+    ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS || "*",
+    DB_URI: process.env.DB_URI || "mongodb://localhost:27017",
+    JWT_SECRET: process.env.JWT_SECRET || "",
+};
