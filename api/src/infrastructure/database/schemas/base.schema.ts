d235348e2c2bd9b9321a30d2fd3a404fe904a64a
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-03-07 13:42:18 -0300

    feature(schemas): updated baseSchema to stop warning

--- api/src/infrastructure/database/schemas/base.schema.ts
+++ api/src/infrastructure/database/schemas/base.schema.ts
@@ -1,10 +1,15 @@
 import * as mongoose from 'mongoose';
-import { TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
-import { pre, getModelForClass, Prop, Ref, modelOptions } from '@typegoose/typegoose';
-
+import {TimeStamps} from '@typegoose/typegoose/lib/defaultClasses';
+import {modelOptions, Prop, Severity} from '@typegoose/typegoose';
 import ObjectId = mongoose.Types.ObjectId;
 
+@modelOptions({
+    options: {
+        allowMixed: Severity.ALLOW
+    }
+})
 export class BaseSchema extends TimeStamps {
-    @Prop({ required: true, default: () => (new ObjectId()).toString() })
+    @Prop({required: true, default: () => (new ObjectId()).toString()})
+
     _id: string | undefined;
 }
