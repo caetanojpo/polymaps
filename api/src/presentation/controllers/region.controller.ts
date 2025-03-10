606cc08723eecb53736e93eeac640a7f2d3f71a3
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-03-10 15:58:21 -0300

    feature(regions): implemented new methods on controller (list, update, delete)

--- api/src/presentation/controllers/region.controller.ts
+++ api/src/presentation/controllers/region.controller.ts
@@ -8,14 +8,28 @@ import {logger} from "../../config/logger";
 import STATUS_CODE from "../../utils/status-code";
 import {ApiResponse} from "../../utils/api-response";
 import {UserRepository} from "../../infrastructure/database/repositories/user.repository";
+import {Coordinates} from "../../domain/types/coordinates.type";
+import {ListRegionsContainingPointDto} from "../../application/dtos/region/list-regions-containing-point.dto";
+import {FindRegionUseCase} from "../../application/use-cases/region/find-region.use-case";
+import {DeleteRegionUseCase} from "../../application/use-cases/region/delete-region.use-case";
+import {UpdateRegionUseCase} from "../../application/use-cases/region/update-region.use-case";
+import {ListRegionsNearPointDTO} from "../../application/dtos/region/list-regions-near-point.dto";
+import {RegionMapper} from "../../infrastructure/mapper/region.mapper";
+import {UpdateRegionDto} from "../../application/dtos/region/update-region.dto";
 
 export class RegionController {
     private create: CreateRegionUseCase;
+    private find: FindRegionUseCase;
+    private update: UpdateRegionUseCase;
+    private delete: DeleteRegionUseCase;
 
     constructor() {
         const regionRepository = RegionRepository.getInstance();
         const userRepository = UserRepository.getInstance();
         this.create = new CreateRegionUseCase(regionRepository, userRepository);
+        this.find = new FindRegionUseCase(regionRepository);
+        this.update = new UpdateRegionUseCase(regionRepository, userRepository);
+        this.delete = new DeleteRegionUseCase(regionRepository);
     }
 
     public async createRegion(req: Request, res: Response, next: NextFunction): Promise<any> {
@@ -28,6 +42,98 @@ export class RegionController {
             const createdRegion = await this.create.execute(regionData);
 
             return res.status(STATUS_CODE.CREATED).json(ApiResponse.success("Region created", {id: createdRegion?._id}));
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    public async findById(req: Request, res: Response, next: NextFunction): Promise<any> {
+        try {
+            const id = req.params.region;
+            const region = await this.find.executeById(id);
+
+            const mappedRegion = RegionMapper.toRegionResponseFromDomain(region!);
+
+            res.status(STATUS_CODE.OK).json(ApiResponse.success("Region found", {mappedRegion}));
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    public async findAll(req: Request, res: Response, next: NextFunction): Promise<any> {
+        try {
+            const {ownerId} = req.query as { ownerId?: string };
+
+            const regions = await this.find.executeAll(ownerId);
+            const mappedRegions = regions.map(region => RegionMapper.toRegionResponseFromDomain(region!));
+
+            res.status(STATUS_CODE.OK).json(ApiResponse.success("Regions found", {mappedRegions}));
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    public async listRegionsContainingPoint(req: Request, res: Response, next: NextFunction): Promise<any> {
+        try {
+            const regionData = plainToInstance(ListRegionsContainingPointDto, req.body as Object);
+            const hasErrors = await this.validateErrors(regionData, res);
+            if (hasErrors) return;
+            const coordinates: Coordinates = {latitude: regionData.latitude, longitude: regionData.longitude};
+            const regions = await this.find.executeRegionsContainingPoint(coordinates);
+            const mappedRegions = regions.map(region => RegionMapper.toRegionResponseFromDomain(region!));
+
+            return res.status(STATUS_CODE.OK).json(ApiResponse.success("Regions containing the point", mappedRegions));
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    public async listRegionsNearPoint(req: Request, res: Response, next: NextFunction): Promise<any> {
+        try {
+            const regionData = plainToInstance(ListRegionsNearPointDTO, req.body as object);
+            const {maxDistance = 5000, ownerId} = req.query as { maxDistance?: number, ownerId?: string };
+
+            const hasErrors = await this.validateErrors(regionData, res);
+            if (hasErrors) return;
+
+            const coordinates: Coordinates = {latitude: regionData.latitude, longitude: regionData.longitude};
+            const regions = await this.find.executeRegionsNearPoint(coordinates, maxDistance, ownerId);
+
+            const mappedRegions = regions.map(region => RegionMapper.toRegionResponseFromDomain(region!));
+
+            return res.status(STATUS_CODE.OK).json(ApiResponse.success("Regions near the point", mappedRegions));
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    public async updateRegion(req: Request, res: Response, next: NextFunction): Promise<any> {
+        try {
+            const id = req.params.regionId
+            const regionData = plainToInstance(UpdateRegionDto, req.body as Object);
+
+            const hasErrors = await this.validateErrors(regionData, res);
+            if (hasErrors) return;
+
+            const updatedRegion = await this.update.execute(id, regionData);
+
+            return res.status(STATUS_CODE.NO_CONTENT);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    public async deleteRegion(req: Request, res: Response, next: NextFunction): Promise<any> {
+        try {
+            const id = req.params.id;
+            const {hardDelete = false} = req.query as { hardDelete?: boolean };
+            if (hardDelete) {
+                await this.delete.executeHard(id);
+            } else {
+                await this.delete.execute(id);
+            }
+
+            return res.status(STATUS_CODE.NO_CONTENT);
         } catch (error) {
             next(error);
         }
