2f2a23ff9cd362289901ddc115fba01b445d186d
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-03-10 11:12:15 -0300

    feat(user): improved user dto validations

--- api/src/application/dtos/users/update-user.dto.ts
+++ api/src/application/dtos/users/update-user.dto.ts
@@ -1,36 +1,39 @@
 import {
-    IsEmail,
+    IsEmail, IsNotEmpty, IsObject,
     IsOptional,
     IsString,
     Matches,
-    MinLength, ValidateIf,
+    MinLength, ValidateIf, ValidateNested,
 } from 'class-validator';
 import {Coordinates} from "../../../domain/types/coordinates.type";
+import {Type} from "class-transformer";
 
 export class UpdateUserDTO {
-    @IsOptional()
-    @IsString()
+    @IsNotEmpty({ message: "Name is required." })
+    @IsString({ message: "Name must be a valid string." })
     name: string;
 
-    @IsOptional()
-    @IsEmail()
+    @IsNotEmpty({ message: "Email is required." })
+    @IsEmail({}, { message: "Invalid email format." })
     email: string;
 
-    @IsOptional()
-    @MinLength(6)
+    @IsNotEmpty({ message: "Password is required." })
+    @MinLength(6, { message: "Password must be at least 6 characters long." })
     @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/, {
-        message:
-            'Password must have at least 6 characters, one uppercase letter, one number, and one special symbol',
+        message: "Password must contain at least one uppercase letter, one number, and one special character."
     })
     password: string;
 
     @ValidateIf((o) => !o.coordinates)
     @IsOptional()
-    @IsString()
+    @IsString({ message: "Address must be a valid string." })
     address?: string;
 
     @ValidateIf((o) => !o.address)
     @IsOptional()
+    @IsObject({ message: "Coordinates must be a valid object." })
+    @ValidateNested()
+    @Type(() => Object)
     coordinates?: Coordinates;
 
     constructor(name: string, email: string, password: string, address?: string, coordinates?: Coordinates) {
