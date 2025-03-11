a98ff7f756dddf3cd2ed22d5b83ac6b05e7e69c2
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-03-11 17:36:50 -0300

    feat(app): tests coverage

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -4,4 +4,14 @@ export default {
     testEnvironment: "node",
     moduleFileExtensions: ["ts", "js", "json"],
     testMatch: ["**/__tests__/**/*.test.ts", "**/?(*.)+(spec|test).ts"],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageThreshold: {
+        global: {
+            statements: 80,
+            branches: 80,
+            functions: 80,
+            lines: 80,
+        },
+    },
 };
