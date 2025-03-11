b57243649748073c6e88ea3d62b9345fba72a4a6
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-03-11 16:37:26 -0300

    feat(tests): create region use case

---
+++ api/tests/unit/use-case/regions/create-region.use-case.spec.ts
@@ -0,0 +1,92 @@
+import {CreateRegionUseCase} from "../../../../src/application/use-cases/region/create-region.use-case";
+import {IRegionRepository} from "../../../../src/domain/repositories/iregion.repository";
+import {IUserRepository} from "../../../../src/domain/repositories/iuser.repository";
+import {CreateRegionDto} from "../../../../src/application/dtos/region/create-region.dto";
+import {Region} from "../../../../src/domain/models/region.model";
+import {RegionException} from "../../../../src/domain/exceptions/region.exception";
+
+jest.mock("../../../../src/domain/repositories/iregion.repository");
+jest.mock("../../../../src/domain/repositories/iuser.repository");
+
+describe("CreateRegionUseCase", () => {
+    let regionRepository: jest.Mocked<IRegionRepository>;
+    let userRepository: jest.Mocked<IUserRepository>;
+    let createRegionUseCase: CreateRegionUseCase;
+
+    beforeEach(() => {
+        regionRepository = {
+            save: jest.fn()
+        } as unknown as jest.Mocked<IRegionRepository>;
+
+        userRepository = {
+            findById: jest.fn()
+        } as unknown as jest.Mocked<IUserRepository>;
+
+        createRegionUseCase = new CreateRegionUseCase(regionRepository, userRepository);
+    });
+
+    it("should create a region when owner exists", async () => {
+        const createRegionDto: CreateRegionDto = {
+            name: "Test Region",
+            owner: "valid-user-id",
+            coordinates: [
+                [
+                    [-122.431297, 37.773972],
+                    [-122.431300, 37.773975],
+                    [-122.431305, 37.773980],
+                    [-122.431297, 37.773972]
+                ]
+            ],
+        };
+
+        const user = {
+            email: "test@test.com",
+            name: "test",
+            hashedPassword: "password123",
+            address: "ABC 123"
+        };
+        userRepository.findById.mockResolvedValue(user);
+        const savedRegion = {
+            ...createRegionDto, owner: user, location: {
+                type: "Polygon",
+                coordinates: [
+                    [
+                        [-122.431297, 37.773972],
+                        [-122.431300, 37.773975],
+                        [-122.431305, 37.773980],
+                        [-122.431297, 37.773972]
+                    ]
+                ]
+            },
+            isActive: true
+        } as Region;
+        regionRepository.save.mockResolvedValue(savedRegion);
+
+        const result = await createRegionUseCase.execute(createRegionDto);
+
+        expect(userRepository.findById).toHaveBeenCalledWith("valid-user-id");
+        expect(regionRepository.save).toHaveBeenCalledWith(expect.objectContaining({
+            name: "Test Region",
+            owner: user,
+            location: {type: 'Polygon', coordinates: createRegionDto.coordinates},
+            isActive: true
+        }));
+        expect(result).toEqual(savedRegion);
+    });
+
+    it("should throw an exception when owner does not exist", async () => {
+        const createRegionDto: CreateRegionDto = {
+            name: "Invalid Region",
+            owner: "invalid-user-id",
+            coordinates: [[[0, 0], [1, 1], [1, 0], [0, 0]]]
+        };
+
+        userRepository.findById.mockResolvedValue(null);
+
+        await expect(createRegionUseCase.execute(createRegionDto))
+            .rejects.toThrow(new RegionException("Owner not found"));
+
+        expect(userRepository.findById).toHaveBeenCalledWith("invalid-user-id");
+        expect(regionRepository.save).not.toHaveBeenCalled();
+    });
+});
