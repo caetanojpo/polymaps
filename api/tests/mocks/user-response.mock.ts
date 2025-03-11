1b276e1e0959c656b8af579d084dbe9de188143d
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-03-11 11:47:45 -0300

    feat(tests): fixed user mocks

--- api/tests/mocks/user-response.mock.ts
+++ api/tests/mocks/user-response.mock.ts
@@ -3,8 +3,8 @@ import {Coordinates} from "../../src/domain/types/coordinates.type";
 
 export const userResponseMock: UserResponseDTO = new UserResponseDTO(
     "user123",
-    "John Doe",
-    "john@example.com",
+    "Test User",
+    "test@test.com",
     "123 Main Street",
     {latitude: 40.7128, longitude: -74.0060} as Coordinates,
     new Date("2023-01-01T00:00:00.000Z"),
@@ -14,8 +14,8 @@ export const userResponseMock: UserResponseDTO = new UserResponseDTO(
 export const userResponseMockList: UserResponseDTO[] = [
     new UserResponseDTO(
         "user123",
-        "John Doe",
-        "john@example.com",
+        "Test User",
+        "test@test.com",
         "123 Main Street",
         {latitude: 40.7128, longitude: -74.0060} as Coordinates,
         new Date("2023-01-01T00:00:00.000Z"),
@@ -23,8 +23,8 @@ export const userResponseMockList: UserResponseDTO[] = [
     ),
     new UserResponseDTO(
         "user124",
-        "Jane Doe",
-        "jane@example.com",
+        "Test User 2",
+        "test2@test.com",
         "456 Another Ave",
         {latitude: 41.0000, longitude: -75.0000} as Coordinates,
         new Date("2023-01-03T00:00:00.000Z"),
@@ -32,8 +32,8 @@ export const userResponseMockList: UserResponseDTO[] = [
     ),
     new UserResponseDTO(
         "user125",
-        "Alice Smith",
-        "alice@example.com",
+        "Test User 3",
+        "test3@test.com",
         "789 Example Blvd",
         {latitude: 42.1234, longitude: -76.5678} as Coordinates,
         new Date("2023-02-01T00:00:00.000Z"),
