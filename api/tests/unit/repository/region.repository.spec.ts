1206a00ff35c2219ae68435c81821e79295850a9
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-03-11 18:06:00 -0300

    feat(app): improved region repository tests

--- api/tests/unit/repository/region.repository.spec.ts
+++ api/tests/unit/repository/region.repository.spec.ts
@@ -1,14 +1,14 @@
 import {MongoMemoryServer} from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import {RegionRepository} from "../../../src/infrastructure/database/repositories/region.repository";
-import {RegionModel} from "../../../src/infrastructure/database/schemas/region.schema";
-import {Region} from "../../../src/domain/models/region.model";
-import {RegionMapper} from "../../../src/infrastructure/mapper/region.mapper";
-import {DatabaseException} from "../../../src/domain/exceptions/database.exception";
-import {PolygonModelImpl} from "../../../src/domain/models/polygon.model";
-import {userResponseMock} from "../../mocks/user/user-response.mock";
-import {User} from "../../../src/domain/models/user.model";
-import {RegionException} from "../../../src/domain/exceptions/region.exception";
+import {RegionRepository} from '../../../src/infrastructure/database/repositories/region.repository';
+import {RegionModel, RegionSchema} from '../../../src/infrastructure/database/schemas/region.schema';
+import {Region} from '../../../src/domain/models/region.model';
+import {RegionMapper} from '../../../src/infrastructure/mapper/region.mapper';
+import {DatabaseException} from '../../../src/domain/exceptions/database.exception';
+import {PolygonModelImpl} from '../../../src/domain/models/polygon.model';
+import {userResponseMock} from '../../mocks/user/user-response.mock';
+import {User} from '../../../src/domain/models/user.model';
+import {RegionException} from '../../../src/domain/exceptions/region.exception';
 
 describe('RegionRepository', () => {
     let mongoServer: MongoMemoryServer;
@@ -28,6 +28,7 @@ describe('RegionRepository', () => {
     });
 
     afterEach(async () => {
+        jest.restoreAllMocks();
         await RegionModel.deleteMany({});
     });
 
@@ -35,35 +36,31 @@ describe('RegionRepository', () => {
         const regionId = new mongoose.Types.ObjectId().toHexString();
         const mockRegion = {_id: regionId, name: 'Test Region', owner: 'ownerId'};
 
-        RegionModel.findById = jest.fn().mockReturnValue({
-            populate: jest.fn().mockReturnValue({exec: jest.fn().mockResolvedValue(mockRegion)})
-        });
+        jest.spyOn(RegionModel, 'findById').mockReturnValueOnce({
+            populate: jest.fn().mockReturnValue({
+                exec: jest.fn().mockResolvedValue(mockRegion)
+            })
+        } as any);
 
-        RegionMapper.toDomainFromSchema = jest.fn().mockReturnValue(
-            new Region('Test Region',
-                <PolygonModelImpl><unknown>{
-                    type: "Polygon", coordinates: [
-                        [
-                            [-122.431297, 37.773972],
-                            [-122.431300, 37.773975],
-                            [-122.431305, 37.773980],
-                            [-122.431297, 37.773972]
-                        ]
-                    ],
-                }, <User>userResponseMock, regionId
+        jest.spyOn(RegionMapper, 'toDomainFromSchema').mockReturnValueOnce(
+            new Region(
+                'Test Region',
+                new PolygonModelImpl('Polygon', [[[-122.431297, 37.773972], [-122.431300, 37.773975], [-122.431305, 37.773980], [-122.431297, 37.773972]]]),
+                userResponseMock as unknown as User,
+                regionId
             )
         );
 
         const result = await repository.findById(regionId);
 
         expect(result).toBeDefined();
-        expect(result!._id).toBe(regionId);
+        expect(result?._id).toBe(regionId);
     });
 
     it('should return null if region is not found', async () => {
-        RegionModel.findById = jest.fn().mockReturnValue({
+        jest.spyOn(RegionModel, 'findById').mockReturnValueOnce({
             populate: jest.fn().mockReturnValue({exec: jest.fn().mockResolvedValue(null)})
-        });
+        } as any);
 
         const result = await repository.findById('invalid_id');
 
@@ -71,9 +68,9 @@ describe('RegionRepository', () => {
     });
 
     it('should throw DatabaseException if MongoDB fails', async () => {
-        RegionModel.findById = jest.fn().mockReturnValue({
+        jest.spyOn(RegionModel, 'findById').mockReturnValueOnce({
             exec: jest.fn().mockRejectedValue(new Error('MongoDB error'))
-        });
+        } as any);
 
         await expect(repository.findById('some_id')).rejects.toThrow(DatabaseException);
     });
@@ -83,21 +80,13 @@ describe('RegionRepository', () => {
             {name: 'Region1', owner: 'ownerId', _id: 'id1'},
             {name: 'Region2', owner: 'ownerId', _id: 'id2'}
         ];
-        RegionModel.find = jest.fn().mockReturnValue({
-            exec: jest.fn().mockResolvedValue(mockRegions)
-        });
 
-        RegionMapper.toDomainFromSchema = jest.fn().mockImplementation(region =>
-            new Region(region.name, <PolygonModelImpl><unknown>{
-                type: "Polygon", coordinates: [
-                    [
-                        [-122.431297, 37.773972],
-                        [-122.431300, 37.773975],
-                        [-122.431305, 37.773980],
-                        [-122.431297, 37.773972]
-                    ]
-                ],
-            }, <User>userResponseMock, region._id,)
+        jest.spyOn(RegionModel, 'find').mockReturnValueOnce({
+            exec: jest.fn().mockResolvedValue(mockRegions)
+        } as any);
+
+        jest.spyOn(RegionMapper, 'toDomainFromSchema').mockImplementation(region =>
+            new Region(region.name, new PolygonModelImpl('Polygon', [[[-122.431297, 37.773972], [-122.431300, 37.773975], [-122.431305, 37.773980], [-122.431297, 37.773972]]]), userResponseMock as unknown as User, region._id)
         );
 
         const result = await repository.findAll();
@@ -108,22 +97,29 @@ describe('RegionRepository', () => {
 
     it('should save a new region', async () => {
         const regionId = new mongoose.Types.ObjectId().toHexString();
-        const newRegion = new Region(regionId,
-            <PolygonModelImpl><unknown>{
-                type: "Polygon", coordinates: [
-                    [
-                        [-122.431297, 37.773972],
-                        [-122.431300, 37.773975],
-                        [-122.431305, 37.773980],
-                        [-122.431297, 37.773972]
-                    ]
-                ],
-            }, <User>userResponseMock
-        )
-        newRegion.name = "New Region";
-        const mockSchema = {...newRegion, save: jest.fn().mockResolvedValue(newRegion)};
-        RegionMapper.toSchemaFromDomain = jest.fn().mockReturnValue(mockSchema);
-        RegionModel.prototype.save = jest.fn().mockResolvedValue(mockSchema);
+        const newRegion = new Region(
+            regionId,
+            new PolygonModelImpl('Polygon', [[[-122.431297, 37.773972], [-122.431300, 37.773975], [-122.431305, 37.773980], [-122.431297, 37.773972]]]),
+            userResponseMock as unknown as User
+        );
+        newRegion.name = 'New Region';
+
+        const mockSchema: RegionSchema = {
+            _id: regionId,
+            name: newRegion.name,
+            location: {
+                type: newRegion.location.type,
+                coordinates: newRegion.location.coordinates as number[][][]
+            },
+            owner: newRegion.owner,
+            isActive: newRegion.isActive,
+            createdAt: newRegion.createdAt,
+            updatedAt: newRegion.updatedAt,
+            save: jest.fn().mockResolvedValue(newRegion)
+        } as unknown as RegionSchema;
+
+        jest.spyOn(RegionMapper, 'toSchemaFromDomain').mockReturnValueOnce(mockSchema);
+        jest.spyOn(RegionModel.prototype, 'save').mockResolvedValueOnce(mockSchema);
 
         const result = await repository.save(newRegion);
 
@@ -132,36 +128,68 @@ describe('RegionRepository', () => {
 
     it('should update an existing region', async () => {
         const regionId = new mongoose.Types.ObjectId().toHexString();
-        const updatedRegion = new Region(regionId, <PolygonModelImpl><unknown>{
-            type: "Polygon",
-            coordinates: [[[-122.431297, 37.773972], [-122.431300, 37.773975], [-122.431305, 37.773980], [-122.431297, 37.773972]]]
-        }, <User>userResponseMock);
+        const updatedRegion = new Region(
+            regionId,
+            new PolygonModelImpl('Polygon', [[[-122.431297, 37.773972], [-122.431300, 37.773975], [-122.431305, 37.773980], [-122.431297, 37.773972]]]),
+            userResponseMock as unknown as User
+        );
 
-        RegionMapper.toSchemaFromDomain = jest.fn().mockReturnValue(updatedRegion);
-
-        RegionModel.findByIdAndUpdate = jest.fn().mockReturnValue({
+        jest.spyOn(RegionMapper, 'toSchemaFromDomain').mockReturnValueOnce({
+            _id: regionId,
+            name: updatedRegion.name,
+            location: {
+                type: updatedRegion.location.type,
+                coordinates: updatedRegion.location.coordinates
+            },
+            owner: updatedRegion.owner,
+            isActive: updatedRegion.isActive,
+            createdAt: updatedRegion.createdAt,
+            updatedAt: updatedRegion.updatedAt
+        } as unknown as RegionSchema);
+        jest.spyOn(RegionModel, 'findByIdAndUpdate').mockReturnValueOnce({
             exec: jest.fn().mockResolvedValue(updatedRegion)
-        });
+        } as any);
 
-        await repository.update(regionId, updatedRegion);
+        await expect(repository.update(regionId, updatedRegion)).resolves.toBeUndefined();
     });
 
     it('should throw RegionException if update fails', async () => {
-        RegionModel.findByIdAndUpdate = jest.fn().mockReturnValue({
+        jest.spyOn(RegionModel, 'findByIdAndUpdate').mockReturnValueOnce({
             exec: jest.fn().mockResolvedValue(null)
-        });
+        } as any);
 
-        await expect(repository.update('invalid_id', new Region('invalid_id', <PolygonModelImpl><unknown>{
-            type: "Polygon",
-            coordinates: [[[-122.431297, 37.773972], [-122.431300, 37.773975], [-122.431305, 37.773980], [-122.431297, 37.773972]]]
-        }, <User>userResponseMock))).rejects.toThrow(RegionException);
+        await expect(repository.update('invalid_id', new Region('invalid_id', new PolygonModelImpl('Polygon', [[[-122.431297, 37.773972], [-122.431300, 37.773975], [-122.431305, 37.773980], [-122.431297, 37.773972]]]), userResponseMock as unknown as User))).rejects.toThrow(RegionException);
     });
 
     it('should delete a region', async () => {
-        RegionModel.findByIdAndDelete = jest.fn().mockReturnValue({
+        jest.spyOn(RegionModel, 'findByIdAndDelete').mockReturnValueOnce({
             exec: jest.fn().mockResolvedValue(null)
-        });
+        } as any);
 
         await expect(repository.delete('some_id')).resolves.toBeUndefined();
+    });
+
+    it('should find regions containing a point', async () => {
+        const coordinates = {latitude: -122.431297, longitude: 37.773972};
+
+        jest.spyOn(RegionModel, 'find').mockReturnValueOnce({
+            exec: jest.fn().mockResolvedValue([])
+        } as any);
+
+        const result = await repository.findRegionsContainingPoint(coordinates);
+
+        expect(result).toEqual([]);
+    });
+
+    it('should find regions near a point', async () => {
+        const coordinates = {latitude: -122.431297, longitude: 37.773972};
+
+        jest.spyOn(RegionModel, 'find').mockReturnValueOnce({
+            exec: jest.fn().mockResolvedValue([])
+        } as any);
+
+        const result = await repository.findRegionsNearPoint(coordinates, 1000);
+
+        expect(result).toEqual([]);
     });
 });
