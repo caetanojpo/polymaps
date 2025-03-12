9ae4e068c0d83b560614bf60a8cc20bace26d5bf
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-03-12 14:30:22 -0300

    feat(front): project configurations

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+        turbo: {
+            // Example: adding an alias and custom file extension
+            resolveAlias: {
+                underscore: 'lodash',
+            },
+            resolveExtensions: ['.mdx', '.tsx', '.ts', '.jsx', '.js', '.json'],
+        },
+    },
 };
 
 export default nextConfig;
--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 export default {
     content: [
@@ -8,9 +8,73 @@ export default {
     ],
     theme: {
         extend: {
-            backgroundImage: {
-            },
+            backgroundImage: {},
             colors: {
+                primary: "#00d256",
+                secondary: "#fff",
+                text: "#000",
+                highlight: "#00d256",
+                dark: "#131313",
+                subprimary: {
+                    50: '#e6f9ed',
+                    100: '#ccf3db',
+                    200: '#99e7b7',
+                    300: '#66dba3',
+                    400: '#33cf8f',
+                    500: '#00d256',
+                    600: '#00a846',
+                    700: '#007e36',
+                    800: '#005426',
+                    900: '#002a16',
+                },
+                subsecondary: {
+                    50: '#ffffff',
+                    100: '#ffffff',
+                    200: '#ffffff',
+                    300: '#ffffff',
+                    400: '#ffffff',
+                    500: '#ffffff',
+                    600: '#cccccc',
+                    700: '#999999',
+                    800: '#666666',
+                    900: '#333333',
+                },
+                subtext: {
+                    50: '#e6e6e6',
+                    100: '#cccccc',
+                    200: '#b3b3b3',
+                    300: '#999999',
+                    400: '#808080',
+                    500: '#000000',
+                    600: '#666666',
+                    700: '#4d4d4d',
+                    800: '#333333',
+                    900: '#1a1a1a',
+                },
+                subhighlight: {
+                    50: '#e6f9ed',
+                    100: '#ccf3db',
+                    200: '#99e7b7',
+                    300: '#66dba3',
+                    400: '#33cf8f',
+                    500: '#00d256',
+                    600: '#00a846',
+                    700: '#007e36',
+                    800: '#005426',
+                    900: '#002a16',
+                },
+                subdark: {
+                    50: '#e6e6e6',
+                    100: '#cccccc',
+                    200: '#b3b3b3',
+                    300: '#999999',
+                    400: '#808080',
+                    500: '#131313',
+                    600: '#666666',
+                    700: '#4d4d4d',
+                    800: '#333333',
+                    900: '#1a1a1a',
+                },
             },
         },
     },
