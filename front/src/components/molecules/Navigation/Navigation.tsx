24210f79dd980d792b4ef0208d8ef5b91067a280
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-03-12 23:26:14 -0300

    fix(front): navbar responsivity

--- front/src/components/molecules/Navigation/Navigation.tsx
+++ front/src/components/molecules/Navigation/Navigation.tsx
@@ -14,12 +14,12 @@ export function Navigation() {
     return (
         <nav className="bg-dark shadow-sm text-white w-screen ">
             <div className="max-w-7xl h-full mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
-                <h1 className="text-[2rem] font-semibold ">Region Management</h1>
+                <h1 className="text-xl xl:text-[2rem] font-semibold ">Region Management</h1>
                 <div className="flex items-center space-x-4">
-                    <span className="text-[1.2rem] text-white mr-6">{user?.email}</span>
-                    <Button variant="primary" className='text-white !text-[1.3rem]' onClick={handleSignOut}>
+                    <span className="text-sm xl:text-[1.2rem] text-white mr-6">{user?.email}</span>
+                    <Button variant="primary" className='text-white text-[1rem] xl:!text-[1.3rem]' onClick={handleSignOut}>
                         <>
-                            <LogOut className="w-6 h-6 mr-4"/>
+                            <LogOut className="hidden xl:flex w-6 h-6 mr-4"/>
                             Sign Out
                         </>
                     </Button>
